0cce9e43609eae6104388d8021822b7ac8ccfdb4
neuxsensei <yusufcankucuk64@gmail.com>
2024-07-03 16:36:21 +0300

    admin panel added.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin.admin')
+
+@section('title','Admin Panel Exp')
+
+@section('content')
+
+    @include('admin._content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,17 @@ Route::get('/about',[HomeController::class,'about'])->name('about');
 Route::get('/home',[HomeController::class,'index'])->name('home');
 Route::get('/test/{id}/{name}',[HomeController::class,'test'])->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+'])->name('test');
 
+/*   Buradan aşşağısı admin paneli için    */
+
+Route::get('/admin',[App\Http\Controllers\Admin\HomeController::class,'admin'])->name('admin');
+
+
+
+
+
+
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
