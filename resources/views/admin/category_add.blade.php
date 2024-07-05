50437559b1380772f0a5d562cc9ff1b736ce63d5
neuxsensei <yusufcankucuk64@gmail.com>
2024-07-05 17:42:24 +0300

    Project Updated

--- resources/views/admin/category_add.blade.php
+++ resources/views/admin/category_add.blade.php
@@ -30,7 +30,7 @@
         <!-- Default box -->
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Add Category</h3>
+                <h2 class="card-title">Category Add</h2>
 
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
@@ -40,12 +40,59 @@
                 </div>
             </div>
             <div class="card-body">
-                Add Category Form
+
+                <div class="card card-primary">
+
+                    <!-- form start -->
+                    <form role="form" action="{{route('admin_category_add')}}" method="post">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Parent ID</label>
+                                <input type="text" name="parent_id" class="form-control" id="exampleInputEmail1" placeholder="Parent ID">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Title</label>
+                                <input type="text" name="title" class="form-control" id="exampleInputPassword1" placeholder="Title">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Keywords</label>
+                                <input type="text" name="keywords" class="form-control" id="exampleInputPassword1" placeholder="Keywords">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Description</label>
+                                <input type="text" name="description" class="form-control" id="exampleInputPassword1" placeholder="Description">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Slug</label>
+                                <input type="text" name="slug" class="form-control" id="exampleInputPassword1" placeholder="Slug">
+                            </div>
+
+                            <!-- <div class="form-group">
+                                <label for="exampleInputFile">File input</label>
+
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div> -->
+
+                        </div>
+                        <!-- /.card-body -->
+
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Add Category</button>
+                        </div>
+                    </form>
+
             </div>
             <!-- /.card-body -->
-            <div class="card-footer">
+            <!-- <div class="card-footer">
                 Footer
-            </div>
+            </div> -->
             <!-- /.card-footer-->
         </div>
         <!-- /.card -->
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::middleware('auth')->prefix('admin')->group(function()
     Route::get('/',[App\Http\Controllers\Admin\HomeController::class,'admin'])->name('admin');
     Route::get('/category', [App\Http\Controllers\Admin\CategoryController::class, 'index'])->name('admin_category');
     Route::get('/category/add', [App\Http\Controllers\Admin\CategoryController::class, 'add'])->name('admin_category_add');
-    Route::get('/category/update', [App\Http\Controllers\Admin\CategoryController::class, 'update'])->name('admin_category_update');
+    Route::post('/category/update', [App\Http\Controllers\Admin\CategoryController::class, 'update'])->name('admin_category_update');
     Route::get('/category/delete', [App\Http\Controllers\Admin\CategoryController::class, 'destroy'])->name('admin_category_delete');
     Route::get('/category/show', [App\Http\Controllers\Admin\CategoryController::class, 'show'])->name('admin_category_show');
 });
